7b6f0981e404fd8d2647446998f7d5c95a6300be
Dave Shashwat <daveshashwat46@gmail.com>
2026-04-27 18:58:01 +0530

    ...

---
+++ Files/file6.py
@@ -0,0 +1,14 @@
+# Also add rohit score to a diffrent file named "rohit.txt" same with others
+data = {"rohit":[100,20,121],"Virat":[90,98,78],"Kl":[151,89,7]}
+
+score = 0
+for player in data:
+    print("Player Name :- ",player)
+    for i in range(len(data[player])):
+        file = open(f"{player}.txt","a")
+        file.write(f"Match {i+1}:- {data[player][i]}\n")
+        file.close()
+        print(f"Match {i+1}:- ",data[player][i])
+        score = score + data[player][i]
+
+print("Total:- ",score)
