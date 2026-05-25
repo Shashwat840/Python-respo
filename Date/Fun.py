862f35eea963210145785b63eb89054c9d614a82
Dave Shashwat <daveshashwat46@gmail.com>
2026-05-25 18:57:26 +0530

    .

--- Date/Fun.py
+++ Date/Fun.py
@@ -1,20 +1,20 @@
 import time
 def function1():
-    print("Function 1 is called")
+    print("Function1 is called")
     with open("time.txt", "a") as file:
-        file.write(f"Function 1 called at: {time.strftime('%H:%M:%S')}\n")
+        file.write(f"function1 called at: {time.strftime('%H:%M:%S')}\n")
 def function2():
-    print("Function 2 is called")
+    print("Function2 is called")
     with open("time.txt", "a") as file:
-        file.write(f"Function 2 called at: {time.strftime('%H:%M:%S')}\n")
+        file.write(f"function2 called at: {time.strftime('%H:%M:%S')}\n")
 def function3():
     print("Function 3 is called")
     with open("time.txt", "a") as file:
-        file.write(f"Function 3 called at: {time.strftime('%H:%M:%S')}\n")  
+        file.write(f"function3 called at: {time.strftime('%H:%M:%S')}\n")  
 def function4():
     print("Function 4 is called")
     with open("time.txt", "a") as file:
-        file.write(f"Function 4 called at: {time.strftime('%H:%M:%S')}\n")  
+        file.write(f"function4 called at: {time.strftime('%H:%M:%S')}\n")  
 while True:
     function1()
     time.sleep(1)
